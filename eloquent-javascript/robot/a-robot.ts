7d290809b72abb8443b37b3708452287cf2d9821
Matthew Birch <matthew678532@gmail.com>
2025-01-18 14:55:48 +0000

    Continued with a-robot project exercise

--- eloquent-javascript/robot/a-robot.ts
+++ eloquent-javascript/robot/a-robot.ts
@@ -1,3 +1,5 @@
+// 1. Setting up our state (graph structure)
+
 const roads: string[] = [
   "Alice's House-Bob's House",   "Alice's House-Cabin",
   "Alice's House-Post Office",   "Bob's House-Town Hall",
@@ -83,25 +85,17 @@ let next = first.move("Alice's House");
 // console.log(first.place);
 // // → Post Office
 
-function randomPick(array: string[]) {
-  let choice = Math.floor(Math.random() * array.length);
-  return array[choice];
-}
+// 2. Setting up our robot to traverse the graph
 
-type Robot = {
-  direction: string,
-  memory: null
-};
+// Robot is the function not the output
 
-function randomRobot(state: VillageState, memory: any) {
-  return {direction: randomPick(roadGraph[state.place]), memory: memory};
-}
+type Robot = (state: VillageState, memory: unknown) => ({ direction: string, memory: any });
 
-function runRobot(state: VillageState, robot: (state: VillageState, memory: any) => Robot, memory: any) {
+function runRobot(state: VillageState, robot: Robot, memory: any): number {
   for (let turn = 0;; turn++) {
     if (state.parcels.length == 0) {
       console.log(`Done in ${turn} turns`);
-      break;
+      return turn;
     }
     let action = robot(state, memory);
     state = state.move(action.direction);
@@ -110,5 +104,111 @@ function runRobot(state: VillageState, robot: (state: VillageState, memory: any)
   }
 }
 
+// 3. Traversing our graph (random orientation)
+
+function randomPick(array: string[]) {
+  let choice = Math.floor(Math.random() * array.length);
+  return array[choice];
+}
+
+function randomRobot(state: VillageState, memory: any) {
+  return {direction: randomPick(roadGraph[state.place]), memory: memory};
+}
+
+console.log("========== Random robot");
 runRobot(VillageState.random(), randomRobot, null);
 
+// 4. Traversing the graph (route orientation)
+
+const mailRoute: string[] = [
+  "Alice's House", "Cabin", "Alice's House", "Bob's House",
+  "Town Hall", "Daria's House", "Ernie's House",
+  "Grete's House", "Shop", "Grete's House", "Farm",
+  "Marketplace", "Post Office"
+];
+
+function routeRobot(state: VillageState, memory: any) {
+  if (memory.length == 0) {
+    memory = mailRoute;
+  }
+  return {direction: memory[0], memory: memory.slice(1)};
+}
+
+console.log("========== Route robot");
+runRobot(VillageState.random(), routeRobot, []);
+
+// 5. Traversing the graph (goal orientation)
+
+function findRoute(graph: Graph, from: string, to: string): string[] {
+  let work = [{at: from, route: <string[]>[]}];
+  for (let i = 0; i < work.length; i++) {
+    let {at, route} = work[i];
+    for (let place of graph[at]) {
+      if (place == to) return route.concat(place);
+      if (!work.some(w => w.at == place)) {
+        work.push({at: place, route: route.concat(place)});
+      }
+    }
+  }
+
+  return [];
+}
+
+function goalOrientedRobot({place, parcels}: VillageState, route: any) {
+  if (route.length == 0) {
+    let parcel = parcels[0];
+    if (parcel.place != place) {
+      route = findRoute(roadGraph, place, parcel.place);
+    } else {
+      route = findRoute(roadGraph, place, parcel.address);
+    }
+  }
+  return {direction: route[0], memory: route.slice(1)};
+}
+
+console.log("========== Goal robot");
+runRobot(VillageState.random(), goalOrientedRobot, []);
+
+// 6. Comparing robots
+
+function compareRobots(robot1: Robot, memory1: any, robot2: Robot, memory2: any) {
+  let robot1Count = 0;
+  let robot2Count = 0;
+
+  for (let i = 0; i < 100; i++) {
+    robot1Count += runRobot(VillageState.random(), robot1, memory1);
+    robot2Count += runRobot(VillageState.random(), robot2, memory2);
+  }
+
+  console.log("Robot 1 average number of steps: ", robot1Count / 100);
+  console.log("Robot 2 average number of steps: ", robot2Count / 100);
+}
+
+compareRobots(routeRobot, [], goalOrientedRobot, []);
+
+// 7. Traversing the graph (goal orientation)
+
+function lazyRobot({place, parcels}: VillageState, route: any) {
+  if (route.length == 0) {
+    let routes = parcels.map(parcel => {
+      if (parcel.place != place) {
+        return { route: findRoute(roadGraph, place, parcel.place), pickUp: true };
+      } else {
+        return { route: findRoute(roadGraph, place, parcel.address), pickUp: false};
+      }
+    });
+
+    function score({ route, pickUp }: { route: string[], pickUp: boolean}) {
+      return (pickUp ? 0.5 : 0) - route.length;
+    }
+
+    route = routes.reduce((a, b) => score(a) > score(b) ? a : b).route;
+  }
+
+  return { direction: route[0], memory: route.slice(1) };
+}
+
+console.log("========== Lazy robot");
+runRobot(VillageState.random(), lazyRobot, []);
+
+compareRobots(goalOrientedRobot, [], lazyRobot, []);
